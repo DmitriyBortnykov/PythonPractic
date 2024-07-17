e2e4dbf7a8978c351f2542bb397aa94a3fc54bc0
DmitriyBortnykov <138170996+DmitriyBortnykov@users.noreply.github.com>
2024-07-17 14:45:54 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,16 @@
+import unittest
 
+from main import add
+
+class TestAdd(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_add_zero(self):
+        self.assertEqual(add(0, 0), 0)
+
+if __name__ == "__main__":
+    unittest.main()
